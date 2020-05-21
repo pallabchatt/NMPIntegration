f51e93a02c8c9a14c480784b4de481ed59f66a73
Pallab <pallabchatt@gmail.com>
2020-05-20 21:34:35 -0400

    Initial Commit

---
+++ src/com/tcs/nmp/client/impl/ESBServiceInvokerImpl.java
@@ -0,0 +1,12 @@
+package com.tcs.nmp.client.impl;
+
+import com.tcs.nmp.client.intf.ServiceInvokerIntf;
+
+public class ESBServiceInvokerImpl implements ServiceInvokerIntf {
+
+	@Override
+	public void invoke() {
+
+	}
+
+}
---
+++ src/com/tcs/nmp/client/intf/ServiceInvokerIntf.java
@@ -0,0 +1,7 @@
+package com.tcs.nmp.client.intf;
+
+public interface ServiceInvokerIntf {
+	
+	public void invoke();
+
+}
